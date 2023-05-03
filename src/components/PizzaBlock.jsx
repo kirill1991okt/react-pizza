f9483f00cfaa044bd92587d12141314b0892de59
kirill1991okt <kirill1991okt@gmail.com>
2023-05-03 16:46:35 +0300

    feat: implement logic for choose parameters of pizza

--- src/components/PizzaBlock.jsx
+++ src/components/PizzaBlock.jsx
@@ -1,25 +1,49 @@
-function PizzaBlock() {
+import React from 'react';
+
+function PizzaBlock({ title, imageUrl, price, types, sizes }) {
+  const [activeIndexType, setActiveIndexType] = React.useState(0);
+  const [activeIndexSize, setActiveIndexSize] = React.useState(0);
+
+  const onClickType = (index) => {
+    setActiveIndexType(index);
+  };
+
+  const onClickSize = (index) => {
+    setActiveIndexSize(index);
+  };
+
+  const pizzaTypes = ['тонкое', 'традиционное'];
+
   return (
     <div className='pizza-block'>
-      <img
-        className='pizza-block__image'
-        src='https://dodopizza-a.akamaihd.net/static/Img/Products/Pizza/ru-RU/b750f576-4a83-48e6-a283-5a8efb68c35d.jpg'
-        alt='Pizza'
-      />
-      <h4 className='pizza-block__title'>Чизбургер-пицца</h4>
+      <img className='pizza-block__image' src={imageUrl} alt='Pizza' />
+      <h4 className='pizza-block__title'>{title}</h4>
       <div className='pizza-block__selector'>
         <ul>
-          <li className='active'>тонкое</li>
-          <li>традиционное</li>
+          {types.map((type) => (
+            <li
+              key={type}
+              className={activeIndexType === type ? 'active' : null}
+              onClick={() => onClickType(type)}
+            >
+              {pizzaTypes[type]}
+            </li>
+          ))}
         </ul>
         <ul>
-          <li className='active'>26 см.</li>
-          <li>30 см.</li>
-          <li>40 см.</li>
+          {sizes.map((size, i) => (
+            <li
+              key={size}
+              className={activeIndexSize === i ? 'active' : null}
+              onClick={() => onClickSize(i)}
+            >
+              {size} см.
+            </li>
+          ))}
         </ul>
       </div>
       <div className='pizza-block__bottom'>
-        <div className='pizza-block__price'>от 395 ₽</div>
+        <div className='pizza-block__price'>от {price} ₽</div>
         <div className='button button--outline button--add'>
           <svg
             width='12'
