13befaf09948874288c97b2cf383fed37751b72d
kirill1991okt <kirill1991okt@gmail.com>
2023-05-10 12:44:57 +0300

    feat: work with url

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,13 @@ import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { setCategory, setCurrentPage } from '../redux/slices/filterSlice';
 import axios from 'axios';
+import qs from 'qs';
+import {
+  useNavigate,
+  useSearchParams,
+  useLocation,
+  useParams,
+} from 'react-router-dom';
 
 import Categories from '../components/Categories';
 import Sort from '../components/Sort';
@@ -11,13 +18,14 @@ import Pagination from '../components/Pagination';
 
 const Home = ({ valueSearch }) => {
   const dispatch = useDispatch();
-  const { categoryId, currentPage } = useSelector((state) => state.filter);
-  const sortType = useSelector((state) => state.filter.sort.sortType);
+  const { category, currentPage } = useSelector((state) => state.filter);
+  const sortType = useSelector((state) => state.filter.sort.sortProperty);
 
   const [items, setItems] = React.useState([]);
   const [isLoading, setIsLoading] = React.useState(true);
-
-  const search = valueSearch ? `search=${valueSearch}` : '';
+  const navigate = useNavigate();
+  const [searchParams, setSearchParams] = useSearchParams();
+  const { param } = useParams();
 
   const onChangeCategory = (id) => {
     dispatch(setCategory(id));
@@ -27,25 +35,52 @@ const Home = ({ valueSearch }) => {
     dispatch(setCurrentPage(number));
   };
 
+  const search = valueSearch ? `search=${valueSearch}` : '';
+  const categoryParams = category > 0 ? `category=${category}` : '';
+
+  console.log(useLocation());
+
+  React.useEffect(() => {}, []);
+
   //* Правильная сортировка в 9 уроке.
   React.useEffect(() => {
     axios
       .get(
-        `https://64528bf5bce0b0a0f749efb6.mockapi.io/items?page=${currentPage}&limit=4&${search}`
+        `https://64528bf5bce0b0a0f749efb6.mockapi.io/items?page=${currentPage}&limit=4&${search}&${categoryParams}&sortBy=${sortType}`
       )
       .then((res) => {
         setItems(res.data);
         setIsLoading(false);
         window.scrollTo(0, 0);
       });
-  }, [valueSearch, currentPage]);
+  }, [valueSearch, currentPage, category, sortType]);
+
+  React.useEffect(() => {
+    // const queryString = qs.stringify(
+    //   {
+    //     page: currentPage,
+    //     category,
+    //     sortBy: sortType,
+    //     limit: 4,
+    //   },
+    //   { addQueryPrefix: true }
+    // );
+
+    setSearchParams({
+      page: currentPage,
+      category,
+      sortBy: sortType,
+      limit: 4,
+    });
+    // navigate(queryString);
+  }, [currentPage, category, sortType]);
 
   const skeletons = [...new Array(6)].map((_, i) => <PizzaSkeleton key={i} />);
   const pizzas = items.map((pizza) => <PizzaBlock key={pizza.id} {...pizza} />);
   return (
     <>
       <div className='content__top'>
-        <Categories categoryId={categoryId} setCategoryId={onChangeCategory} />
+        <Categories categoryId={category} setCategoryId={onChangeCategory} />
         <Sort />
       </div>
       <h2 className='content__title'>Все пиццы</h2>
--- src/redux/slices/filterSlice.js
+++ src/redux/slices/filterSlice.js
@@ -1,7 +1,7 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-  category: 1,
+  category: 0,
   currentPage: 1,
   sort: {
     name: 'популярности',
