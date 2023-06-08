07ee1d8d6eba191e25726c71b8209a4ea1c7f10e
kirill1991okt <kirill1991okt@gmail.com>
2023-06-08 10:33:12 +0300

    feat: deploy on GitHub pages

--- src/index.tsx
+++ src/index.tsx
@@ -13,7 +13,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   // <React.StrictMode>
-  <BrowserRouter>
+  <BrowserRouter basename='/react-pizza'>
     <Provider store={store}>
       <App />
     </Provider>
